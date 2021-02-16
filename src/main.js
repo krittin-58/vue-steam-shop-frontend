c70e221d8a982b0f22b3ef5b836d934a5fd24beb
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-02-16 17:08:25 +0700

    install vuikit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import '@vuikit/theme'
+
+Vue.use(Vuikit)
+Vue.use(VuikitIcons)
 
 Vue.config.productionTip = false
 

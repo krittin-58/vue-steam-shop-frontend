d69bb016f53d1ce1d560f074f9670c0efb53872e
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2021-03-19 14:09:25 +0700

    setup firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuikit from 'vuikit'
 import VuikitIcons from '@vuikit/icons'
 import App from './App.vue'
+import firebase from 'firebase'
 import router from './router'
 import store from './store'
 
@@ -12,9 +13,18 @@ Vue.use(VuikitIcons)
 
 Vue.config.productionTip = false
 
-Vue.filter('currency', function (value) {
-  return '฿' + parseFloat(value).toFixed(2);
-});
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbULXuvw7jQW2udCnTpxBhNokqNkpX2KE",
+  authDomain: "vue-steam-shop.firebaseapp.com",
+  projectId: "vue-steam-shop",
+  storageBucket: "vue-steam-shop.appspot.com",
+  messagingSenderId: "868790600334",
+  appId: "1:868790600334:web:cee87df01d05eed51fbfdf",
+  measurementId: "G-YRBZ2LP176"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
